2d5ae6ccbe91d27cc608cb9991ac44d6b892e04e
Kaan <kaankabalak@gmail.com>
2017-08-07 19:16:00 -0700

    added path

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,6 @@
 var notes = require('../controllers/notes.js');
+// require path for getting the models path
+var path = require('path');
 module.exports = function(app) {
     app.get('/notes', function(req, res) {
         // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
