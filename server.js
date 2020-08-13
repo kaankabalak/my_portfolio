1a17c1eede675d312f988322d2f399e17dead67f
Kaan <kaankabalak@gmail.com>
2020-08-13 01:11:27 -0700

    Get rid of unused code

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var app = express();
 // Require body-parser (to receive post data from clients)
 var bodyParser = require('body-parser');
 // Integrate body-parser with our App
-// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Setting our Static Folder Directory
 app.use(express.static(path.join(__dirname, '/public/dist')));
@@ -15,8 +14,6 @@ app.use(express.static(path.join(__dirname, '/public/dist')));
 app.set('views', path.join(__dirname, './client/views'));
 // Setting our View Engine set to EJS
 app.set('view engine', 'ejs');
-// require the mongoose configuration file which does the rest for us
-require('./server/config/mongoose.js');
 // store the function in a variable
 var routes_setter = require('./server/config/routes.js');
 // invoke the function stored in routes_setter and pass it the "app" variable
